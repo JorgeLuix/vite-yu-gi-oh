e21c9ccf7b3bf31e5e4b0622876fec49432e0c89
JorgeLuix <tec.jorge.29@gmai.com>
2023-04-18 11:57:55 +0200

    aggiunta select su App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,15 @@
   <div class="header">
     <HeaderComponent/>
   </div>
-  <div class=" container select">selecttype</div>
+  <div class="container">
+  <select v-model="selectedArchetype">
+    <option value="">All</option>
+    <option v-for="archetype in archetypes" :key="archetype" :value="archetype">{{ archetype }}</option>
+  </select>
+</div>
   <div class="container p-5 bg-black mt-3">
      <main>
-        <CardGridComponent :cards="cards">
+        <CardGridComponent :cards="filteredCards">
         </CardGridComponent>
     </main>
   </div>
@@ -16,17 +21,37 @@ import axios from 'axios';
 import CardGridComponent from './components/CardGridComponent.vue';
 import HeaderComponent from './components/headerComponent.vue';
 
+
 export default {
   name: 'App',
   components: {
     CardGridComponent,
-    HeaderComponent
+    HeaderComponent,
+    
 },
   data() {
     return {
       cards: [],
+      selectedArchetype:'',
     };
   },
+  computed: {
+  archetypes() {
+    const archetypes = new Set();//crea un nuovo oggetto Set vuoto, per tenere traccia degli archetipi.
+    this.cards.forEach((card) => {
+      if (card.archetype) {
+        archetypes.add(card.archetype);
+      }
+    });
+    return Array.from(archetypes);
+  },
+  filteredCards() {
+    if (!this.selectedArchetype) {
+      return this.cards;
+    }
+    return this.cards.filter((card) => card.archetype === this.selectedArchetype);
+  },
+},
   methods: {
     getCards() {
       axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=50&offset=0')
